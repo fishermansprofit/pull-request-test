5b9718ae4063db7dafa97c791d1e280f619282db
fishermansprofit <fishermansprofit@gmail.com>
2017-09-26 12:53:13 +0900

    配列をソートする処理を追加

--- sort.js
+++ sort.js
@@ -1,2 +1,12 @@
+var sort_number = function (number) {
+	number.sort(function (a, b) {
+		if (a == b) {
+			return 0;
+		}
+		return a < b ? -1 : 1;
+	});
+};
+
 var number = [19, 3, 81, 1, 24, 21];
+sort_number(number);
 console.log(number);
